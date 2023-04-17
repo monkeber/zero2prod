b1ba187e749b9b0473b4437967199189645065ce
Monkeber <27787571+monkeber@users.noreply.github.com>
2023-04-18 01:44:33 +0300

    1. Removed unnecessary reference. 2. Replaced connect with connect_lazy so the app does not crash right after the start.

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection =
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to the port");
     run(listener, connection)?.await?;
