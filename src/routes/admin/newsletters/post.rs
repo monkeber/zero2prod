16302deca5a06cd8bddf0f1123c5a95ce17f3d2c
Monkeber <27787571+monkeber@users.noreply.github.com>
2023-09-26 23:52:36 +0300

    Added persistence module for saving and retrieving saved responses with the corresponding idempotency keys.

--- src/routes/admin/newsletters/post.rs
+++ src/routes/admin/newsletters/post.rs
@@ -1,6 +1,7 @@
+use crate::authentication::UserId;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
-use crate::idempotency::IdempotencyKey;
+use crate::idempotency::{get_saved_response, save_response, IdempotencyKey};
 use crate::routes::error_chain_fmt;
 use crate::utils::{e400, e500, see_other};
 use actix_web::{http::StatusCode, web, HttpResponse, ResponseError};
@@ -47,14 +48,24 @@ pub async fn publish_newsletter(
     body: web::Form<FormData>,
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
+    user_id: web::ReqData<UserId>,
 ) -> Result<HttpResponse, actix_web::Error> {
+    let user_id = user_id.into_inner();
     let FormData {
         title,
         text_body,
         html_body,
         idempotency_key,
     } = body.0;
-    let _idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
+    let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
+    if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, *user_id)
+        .await
+        .map_err(e500)?
+    {
+        FlashMessage::info("The newsletter issue has been published!").send();
+        return Ok(saved_response);
+    }
+
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
     for subscriber in subscribers {
         match subscriber {
@@ -76,7 +87,13 @@ pub async fn publish_newsletter(
         }
     }
     FlashMessage::info("The newsletter issue has been published!").send();
-    Ok(see_other("/admin/newsletters"))
+
+    let response = see_other("/admin/newsletters");
+    let response = save_response(&pool, &idempotency_key, *user_id, response)
+        .await
+        .map_err(e500)?;
+
+    Ok(response)
 }
 
 struct ConfirmedSubscriber {
