4485184b1d0ce69596cd5ef3735e17949056a3f8
Monkeber <27787571+monkeber@users.noreply.github.com>
2023-10-04 01:59:26 +0300

    1. Removed not null constraints on some columns in idempotency table. 2. Added test for checking the behavior during several concurrent incoming requests. 3. Started implementing logic for the aforementioned use-case.

--- src/routes/admin/newsletters/post.rs
+++ src/routes/admin/newsletters/post.rs
@@ -1,7 +1,7 @@
 use crate::authentication::UserId;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
-use crate::idempotency::{get_saved_response, save_response, IdempotencyKey};
+use crate::idempotency::{save_response, try_processing, IdempotencyKey, NextAction};
 use crate::routes::error_chain_fmt;
 use crate::utils::{e400, e500, see_other};
 use actix_web::{http::StatusCode, web, HttpResponse, ResponseError};
@@ -39,6 +39,10 @@ pub struct FormData {
     idempotency_key: String,
 }
 
+fn success_message() -> FlashMessage {
+    FlashMessage::info("The newsletter issue has been published!")
+}
+
 #[tracing::instrument(
     name = "Publish a newsletter issue",
     skip(body, pool, email_client),
@@ -58,12 +62,16 @@ pub async fn publish_newsletter(
         idempotency_key,
     } = body.0;
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
-    if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, *user_id)
+
+    match try_processing(&pool, &idempotency_key, *user_id)
         .await
         .map_err(e500)?
     {
-        FlashMessage::info("The newsletter issue has been published!").send();
-        return Ok(saved_response);
+        NextAction::StartProcessing => {}
+        NextAction::ReturnSavedResponse(saved_response) => {
+            success_message().send();
+            return Ok(saved_response);
+        }
     }
 
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
@@ -86,7 +94,7 @@ pub async fn publish_newsletter(
             }
         }
     }
-    FlashMessage::info("The newsletter issue has been published!").send();
+    success_message().send();
 
     let response = see_other("/admin/newsletters");
     let response = save_response(&pool, &idempotency_key, *user_id, response)
